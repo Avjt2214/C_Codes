af30e0eb00ff8227b0c730dac5a59a8e8d175cd2
Abhijit Pasari <101591976+Avjt2214@users.noreply.github.com>
2022-03-26 13:22:45 +0530

    Add files via upload

---
+++ PrintingNameB'DayMob.c
@@ -0,0 +1,10 @@
+//Write a C program to print your name, date of birth. and mobile number.
+
+#include<stdio.h>
+
+void main()
+{
+	printf("Name: Abhijit Pasari\n");
+	printf("DOB: May 25 1975\n");
+	printf("Mobile number: +91 xxxxxxxx83\n");
+}
